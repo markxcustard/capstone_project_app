040d6e3cd820848126f7c932b5ef055d74d3846d
Mark Custard <markc1772@gmail.com>
2017-12-13 17:32:38 -0600

    Restful API routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :v1 do
+    get "/users" => "users#index"
+    post "/users" => "users#create"
+    get "/users/:id" => "users#show"
+    patch "/users/:id" => "users#update"
+    delete "/users/:id" => "users#destroy"
+  end
 end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
