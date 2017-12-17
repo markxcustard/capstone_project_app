813bdb25508643e7e06abe2b738f83bee6bc4709
Mark Custard <markc1772@gmail.com>
2017-12-17 15:06:16 -0600

    Added Exercise info to seeds file

--- app/frontend.rb
+++ app/frontend.rb
@@ -2,4 +2,13 @@ require "unirest"
 require "pp"
 
 puts "Welcome to the CrossFit app!! Select an option"
-puts "[1] Show "
+puts "[1] Show All Exercises: "
+puts "[2] Show ALl Workouts: "
+puts "[3] Show One Exercise: "
+puts "[4] Show One Workout: "
+puts "[5] Generate Random Workout: "
+puts "[6]...................."
+input_option = gets.chomp
+if input_option == "1"
+  
+end
