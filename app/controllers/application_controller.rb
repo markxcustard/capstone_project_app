7bb7371a96b6d023dedca59e7044164d8603ad46
Mark Custard <markc1772@gmail.com>
2017-12-20 20:27:51 -0600

    Removed CRUD from Application Controller doh and put in Users Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,33 +1,3 @@
 class ApplicationController < ActionController::API
-  def index
-    users = User.all
-    render json: users.as_json
-  end
-  def create
-    user = User.new(
-      name: params[:name],
-      email: params[:email],
-      password: params[:password]
-      )
-    user.save
-    render json: user.as_json
-  end
-  def show
-    user = User.find_by(id: params[:id])
-    render json: user.as_json
-  end
-  def update
-    user = User.find_by(id: params[:id])
-    user.name = params[:name] || user.name
-    user.email = params[:email] || user.email
-    user.password = params[:password] || user.password
-    user.save
-    render json: user.as_json
-  end
-  def destroy
-    user = User.find_by(id: params[:id])
-    user.destroy
-    render json: {message: "User Successfully Removed!!!"}
-  end
-
 end
+    
--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -11,5 +11,18 @@ class V1::UsersController < ApplicationController
     else
       render json: {errors: user.errors.full_messages}, status: :bad_request
     end
+    def update
+      user = User.find_by(id: params[:id])
+      user.name = params[:name] || user.name
+      user.email = params[:email] || user.email
+      user.password = params[:password] || user.password
+      user.save
+      render json: user.as_json
+    end
+    def destroy
+      user = User.find_by(id: params[:id])
+      user.destroy
+      render json: {message: "User Successfully Removed!!!"}
+    end
   end
 end
